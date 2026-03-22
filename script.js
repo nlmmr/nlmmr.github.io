4265f005fa331863afd76a7ea405e3d331552a76
Nicolas Leandro de Melo Marques <nicolaslmmr@gmail.com>
2026-03-22 15:40:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+
+<!-- =========================
+script.js
+========================= -->
+
+// LISTA DE ARQUIVOS (adicione seus PDFs aqui)
+const files = [
+  {
+    name: "Exemplo PDF 1",
+    path: "files/exemplo1.pdf"
+  },
+  {
+    name: "Exemplo PDF 2",
+    path: "files/exemplo2.pdf"
+  }
+];
+
+const container = document.getElementById("file-list");
+
+function loadFiles() {
+  files.forEach(file => {
+    const div = document.createElement("div");
+    div.className = "file-card";
+
+    div.innerHTML = `
+      <h3>${file.name}</h3>
+      <a href="${file.path}" download>
+        <button>Baixar</button>
+      </a>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+loadFiles();
